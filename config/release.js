bac00c29fc552932825867f6a2c6f4193e422671
Erika Pauwels <erika.pauwels@gmail.com>
2016-05-11 15:06:05 +0200

    Update release process

--- config/release.js
+++ config/release.js
@@ -31,26 +31,6 @@ module.exports = {
         zip.addLocalFolder( path.join(project.root, "dist"), "dist" );
         zip.writeZip( path.join(project.root, "dist.zip") );
         console.log('[TODO] Please upload ' + path.join(project.root, "dist.zip") + ' to the GitHub release once the tag is pushed and maybe trigger the build on hub.docker.com again.');
-
-        // update version in docker-compose.yml
-        console.log("Updating docker-compose.yml with new version...");
-        var composePath = path.join(project.root, 'docker-compose.yml');
-        var nextDockerVersion = tags.next.slice(1);
-        fs.readFile(composePath, 'utf8', function (err,data) {
-          if (err) {
-            return failure(err);
-          }
-          var result = data.replace(/bde2020\/pipeline-builder:.*/g, 'bde2020/pipeline-builder:' + nextDockerVersion);
-
-          fs.writeFile(composePath, result, 'utf8', function (err) {
-            if (err) {
-              console.log(err);
-              failure(err);
-            } else {
-              success();
-            }
-          });
-        });
       }, failure );
     });
   }

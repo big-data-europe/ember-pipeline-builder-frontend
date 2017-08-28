6e1b7734f3d54c576b4321a894ab6d4a3da6828d
Nóra Szepes <nora.szepes@tenforce.com>
2017-08-28 14:35:12 +0200

    updated to ember 2.14.2; updated sass and fixed decaff error

--- app/routes/pipelines/steps.js
+++ app/routes/pipelines/steps.js
@@ -1,16 +1,7 @@
-import Ember from `import Ember from 'ember'`
-
-PipelinesStepsRoute = Ember.Route.extend
-  model: (params) ->
-    @store.findRecord 'pipeline', params.pipeline_id
-
-`export default PipelinesStepsRoute`
-'ember';
-
+import Ember from 'ember';
 const PipelinesStepsRoute = Ember.Route.extend({
   model(params) {
     return this.store.findRecord('pipeline', params.pipeline_id);
   }
 });
-
 export default PipelinesStepsRoute;

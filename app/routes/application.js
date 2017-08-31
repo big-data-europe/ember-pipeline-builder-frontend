849f895940566c2ee8680554f43db57afebfdbcc
Esteban <esteban.sastre@tenforce.com>
2017-08-31 15:36:39 +0200

    Fixed build crash removing ember-key-responder & added default route transition

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 import LoadingSliderMixin from 'ember-cli-loading-slider/mixins/loading-slider';
 
-const ApplicationRoute = Ember.Route.extend(LoadingSliderMixin);
+const ApplicationRoute = Ember.Route.extend(LoadingSliderMixin, {
+    beforeModel() {
+        this.transitionTo('pipelines.index');
+    }
+});
 
 export default ApplicationRoute;
